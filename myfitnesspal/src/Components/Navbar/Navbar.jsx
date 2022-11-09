d1d869ec9279d59aceefcdb4806cc61b7d2bcc27
omchand <omchandjoker27@gmail.com>
2022-11-09 20:12:03 +0530

    day-2

---
+++ myfitnesspal/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+const Navbar = () =>{
+
+    return <div>
+        <div>
+            <h1>myfitnesspal</h1>
+        </div>
+        <div>
+            <h1>LOG IN</h1>
+        </div>
+    </div>
+
+}
+
+export default Navbar;
