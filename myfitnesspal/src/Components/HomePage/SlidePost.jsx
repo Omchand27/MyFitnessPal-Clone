b3383d7a3da309cc99d5a824f8d25577bd8abdf4
omchand <omchandjoker27@gmail.com>
2022-11-10 23:46:57 +0530

    day-3 last

--- myfitnesspal/src/Components/HomePage/SlidePost.jsx
+++ myfitnesspal/src/Components/HomePage/SlidePost.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import Title from "./Title";
 
 const SlidePost = () => {
@@ -10,6 +10,7 @@ const SlidePost = () => {
   return (
     <Box mt="9%">
       <Title Titles={Titles} />
+      <Button mt="5%" colorScheme="blue" size="lg">START YOUR JOURNEY TODAY</Button>
     </Box>
   );
 };
